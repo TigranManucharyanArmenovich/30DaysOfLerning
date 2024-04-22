f6c2a8fbffceb7d7cabf8cb43224c761c6060200
Tigran <80146643+TigranManucharyanArmenovich@users.noreply.github.com>
2024-04-22 12:47:57 +0300

    day5 ver0.1

--- Oculich_Programming/src/CodeMuJava/Level4/Work1.java
+++ Oculich_Programming/src/CodeMuJava/Level4/Work1.java
@@ -2,7 +2,7 @@ package CodeMuJava.Level4;
 
 public class Work1 {
     public static void main(String[] args) {
-        Work2.Exercise work = new Work2.Exercise();
+        Exercise work = new Exercise();
 
         System.out.println();
         work.ex1();
@@ -10,8 +10,19 @@ public class Work1 {
 
     public static class  Exercise{
         public  void ex1(){
-            System.out.println("Задание 1");
-            System.out.println(" ");
+            System.out.println("Задание 12");
+            System.out.println("Дана некоторая строка с буквами и цифрами. Получите позицию первой цифры в этой строке");
+
+            String str = "arae23sfsd";
+            System.out.println("Строка для примера: " + str);
+
+            for (int i = 0; i < str.length(); i++){
+                //System.out.println(str.charAt(i) + " | " + i);
+                if ((int)str.charAt(i) >= 48 && (int)str.charAt(i) <= 57){
+                    System.out.println("Позиция первой цифры в строке: " + i);
+                    break;
+                }
+            }
         }
 
     }
--- Oculich_Programming/src/CodeMuJava/Level4/Work2.java
+++ Oculich_Programming/src/CodeMuJava/Level4/Work2.java
@@ -10,7 +10,22 @@ public class Work2 {
     public static class  Exercise{
         public  void ex1(){
             System.out.println("Задание 1");
-            System.out.println(" ");
+            System.out.println("Дано целое число. Выведите в консоль первую цифру этого числа.");
+
+            System.out.println();
+
+            int OriginalNumber = 22345;
+            int number = OriginalNumber;
+            int reversedNumber = 0;
+
+            while (number != 0) {
+                reversedNumber = reversedNumber * 10 + number % 10;
+                number /= 10;
+            }
+
+            
+            System.out.println("Число для примера: " + OriginalNumber);
+            System.out.println("Первое число: " + reversedNumber % 10);
         }
     }
 }
