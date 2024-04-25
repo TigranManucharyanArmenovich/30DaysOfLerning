8df88dcd66dd90f57a4d6dbb5180233147473377
Tigran <80146643+TigranManucharyanArmenovich@users.noreply.github.com>
2024-04-26 01:25:24 +0300

    day 8 ver 0.3

--- Georg_dudar_java_Games_cource/src/main/java/org/example/georg_dudar_java_games_cource/HelloController.java
+++ Georg_dudar_java_Games_cource/src/main/java/org/example/georg_dudar_java_games_cource/HelloController.java
@@ -1,14 +1,22 @@
 package org.example.georg_dudar_java_games_cource;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button MainBut;
+
+    @FXML
+    private Label MainLabel;
+
+    @FXML
+    void ShowText(ActionEvent event) {
+        MainLabel.setText("Какой хороший маааальчик");
+
     }
+
 }
