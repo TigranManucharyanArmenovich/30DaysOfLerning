3aea3d94d92515db902a1fcec12b85040f40cb2a
Tigran <80146643+TigranManucharyanArmenovich@users.noreply.github.com>
2024-04-26 00:53:51 +0300

    day 8 ver 0.1

--- Georg_dudar_java_Games_cource/src/main/java/org/example/georg_dudar_java_games_cource/HelloController.java
+++ Georg_dudar_java_Games_cource/src/main/java/org/example/georg_dudar_java_games_cource/HelloController.java
@@ -1,34 +1,14 @@
 package org.example.georg_dudar_java_games_cource;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
+    @FXML
+    private Label welcomeText;
 
     @FXML
-    private ResourceBundle resources;
-
-    @FXML
-    private URL location;
-
-    @FXML
-    private Label CoreLabel;
-
-    @FXML
-    private Button MainButton;
-
-    @FXML
-    void BtnClick(ActionEvent event) {
-        CoreLabel.setText("Good boy!");
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
     }
-
-    @FXML
-    void initialize() {
-
-    }
-
 }
